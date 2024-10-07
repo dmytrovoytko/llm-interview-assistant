9ab648441b43e21d9181a7dffb773acc155e7d67
dmytrovoytko <dmytro.voytko@gmail.com>
2024-10-08 02:42:30 +0300

    Grafana

---
+++ interview_assistant/init_gr.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo
+echo
+
+if [[ -e ".env" ]]
+  then
+    # loading script parameters from .env
+    set -a            
+    source .env
+    set +a
+else
+    echo "No .env file with paramaters found. Exiting."
+    exit 1
+fi
+
+echo
+echo '2. Executing init_gr.py'
+echo
+docker exec -it streamlit bash -c "python init_gr.py"
+
+sleep 1
+
+echo
+echo 'Now you can open Grafana (port 3000), default login,pass = admin, admin'
+echo
+
+sleep 5
+

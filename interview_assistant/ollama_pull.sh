6ba0f8dfe8d89c6b9b71b78c8f5761a6fcae544b
dmytrovoytko <dmytro.voytko@gmail.com>
2024-10-08 03:14:10 +0300

    Ollama qwen2.5:3b

--- interview_assistant/ollama_pull.sh
+++ interview_assistant/ollama_pull.sh
@@ -8,3 +8,9 @@ echo
 echo 'Pulling ollama phi3.5 model'
 echo
 docker exec -it ollama ollama pull phi3.5
+
+echo
+echo 'Pulling ollama qwen2.5:3b model disabled by default, just uncomment the line below'
+echo
+# docker exec -it ollama ollama pull qwen2.5:3b
+
